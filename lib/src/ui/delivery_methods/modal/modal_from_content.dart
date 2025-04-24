212dcd83a5cad8d14b97e648c24f932ddcf1684f
Renat Shakhmatov <shushper@gmail.com>
2025-04-24 23:23:23 +0500

    Send content events for modal.

--- lib/src/ui/delivery_methods/modal/modal_from_content.dart
+++ lib/src/ui/delivery_methods/modal/modal_from_content.dart
@@ -1,59 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:gravity_sdk/src/ui/elements/gravity_element.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 import '../../../models/content.dart';
+import '../../../utils/events_service.dart';
 
-class ModalFromContent extends StatelessWidget {
+class ModalFromContent extends StatefulWidget {
   final Content content;
 
   const ModalFromContent({super.key, required this.content});
 
   @override
+  State<ModalFromContent> createState() => _ModalFromContentState();
+}
+
+class _ModalFromContentState extends State<ModalFromContent> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ContentEventsService.instance.sendContentImpression(widget.content);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final frameUi = content.variables.frameUI!;
+    final frameUi = widget.content.variables.frameUI!;
     final container = frameUi.container;
     final close = frameUi.close;
-    final elements = content.variables.elements;
-    final events = content.events;
+    final elements = widget.content.variables.elements;
+    final events = widget.content.events;
+    final templateId = widget.content.templateId;
 
-    return Dialog(
-      backgroundColor: container.style.backgroundColor ?? Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(container.style.cornerRadius ?? 0),
-      ),
-      child: Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-              left: container.style.padding?.left ?? 0,
-              top: container.style.padding?.top ?? 0,
-              right: container.style.padding?.right ?? 0,
-              bottom: container.style.padding?.bottom ?? 0,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: elements
-                  .map(
-                    (e) => GravityElement(element: e, events: events).getWidget(),
-                  )
-                  .toList(),
-            ),
-          ),
-          if (close != null)
-            Positioned(
-              left: close.style.positioned?.left,
-              top: close.style.positioned?.top,
-              right: close.style.positioned?.right,
-              bottom: close.style.positioned?.bottom,
-              child: IconButton(
-                icon: close.image != null
-                    ? Image.network(close.image!, width: close.style.size?.width, height: close.style.size?.height)
-                    : Icon(Icons.close),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+    return VisibilityDetector(
+      key: ValueKey(templateId),
+      onVisibilityChanged: (info) {
+        var visiblePercentage = info.visibleFraction * 100;
+        if (visiblePercentage >= 50) {
+          ContentEventsService.instance.sendContentVisibleImpression(widget.content);
+        }
+      },
+      child: Dialog(
+        backgroundColor: container.style.backgroundColor ?? Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(container.style.cornerRadius ?? 0),
+        ),
+        child: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                left: container.style.padding?.left ?? 0,
+                top: container.style.padding?.top ?? 0,
+                right: container.style.padding?.right ?? 0,
+                bottom: container.style.padding?.bottom ?? 0,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: elements
+                    .map(
+                      (e) => GravityElement(element: e, events: events).getWidget(),
+                    )
+                    .toList(),
               ),
             ),
-        ],
+            if (close != null)
+              Positioned(
+                left: close.style.positioned?.left,
+                top: close.style.positioned?.top,
+                right: close.style.positioned?.right,
+                bottom: close.style.positioned?.bottom,
+                child: IconButton(
+                  icon: close.image != null
+                      ? Image.network(close.image!, width: close.style.size?.width, height: close.style.size?.height)
+                      : Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    ContentEventsService.instance.sendContentClosed(widget.content);
+                  },
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
